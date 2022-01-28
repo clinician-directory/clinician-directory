f76f169bc38da184ba9140894a679cb1897a2771
whoguy24 <whoguy24@gmail.com>
2022-01-28 09:28:24 -0600

    SPIKE

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import {React, Component} from 'react';
+
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -6,9 +10,21 @@ import React from 'react';
 // or even care what the redux state is
 
 function InfoPage() {
+
+  const myApiKey = process.env.API_KEY;
+const calendarId = 1
+
   return (
     <div className="container">
       <p>Info Page</p>
+        <FullCalendar 
+          plugins={[timeGridPlugin, googleCalendarPlugin]}
+          googleCalendarApiKey={myApiKey}
+          weekends={true}
+          slotMinTime={'08:00:00'}
+          slotMaxTime={'22:00:00'}
+          events={calendarId}
+        />
     </div>
   );
 }
