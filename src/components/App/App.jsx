b23b1dbf82001bbc59a44736ae46d8a2b4d7294d
whoguy24 <whoguy24@gmail.com>
2022-02-01 12:55:35 -0600

    Added login functionality to the login page.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,12 +16,13 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import CalendarMonth from '../CalendarMonth/CalendarMonth';
-import LandingPage from '../LandingPage/LandingPage';
-import LoginPage from '../LoginPage/LoginPage';
-import RegisterPage from '../RegisterPage/RegisterPage';
+// import LandingPage from '../LandingPage/LandingPage';
+// import LoginPage from '../LoginPage/LoginPage';
+// import RegisterPage from '../RegisterPage/RegisterPage';
 import ChooseProvider from '../ChooseProvider/ChooseProvider';
 
 import LoginView from '../LoginView/LoginView';
+import TopAppBar from '../TopAppBar/TopAppBar';
 
 import './App.css';
 
@@ -38,63 +39,58 @@ function App() {
     <Router>
       <div>
         {/* <Nav /> */}
+        <TopAppBar/>
         <Switch>
           {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-          <Redirect exact from="/" to="/home" />
+          <Redirect exact from='/' to='/user' />
 
           {/* Visiting localhost:3000/about will show the about page. */}
-          <Route
+          {/* <Route
             // shows AboutPage at all times (logged in or not)
             exact
             path="/about"
           >
             <AboutPage />
-          </Route>
+          </Route> */}
 
           {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
-            <UserPage />
-          </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
-            exact
-            path="/calendarmonth"
-          >
-            <CalendarMonth />
-          </ProtectedRoute>
-
-
-          <ProtectedRoute
-            // logged in shows ChooseProvider else shows LoginPage
-            exact
-            path="/chooseprovider"
-          >
-            <ChooseProvider />
-          </ProtectedRoute>
-
-          <Route
-            exact
-            path="/login"
-          >
+            {/* USER/PROFILE VIEW */}
+          <ProtectedRoute exact path='/user'>
             {user.id ?
-              // If the user is already logged in,
-              // redirect to the /user page
-              <Redirect to="/user" />
+              <UserPage/>
               :
-              // Otherwise, show the login page
-              <LoginView />
+              <Redirect to='/login' />
             }
+          </ProtectedRoute>
+
+            {/* CALENDAR VIEW - MONTH */}
+          <ProtectedRoute exact path='/calendarmonth'>
+            {user.id ?
+              <CalendarMonth />
+              :
+              <Redirect to='/login' />
+            }
+          </ProtectedRoute>
+
+            {/* PROVIDER VIEW */}
+          <ProtectedRoute exact path='/chooseprovider'>
+            {user.id ?
+              <ChooseProvider />
+              :
+              <Redirect to='/login' />
+            }
+          </ProtectedRoute>
+
+            {/* LOGIN VIEW */}
+          <Route exact path='/login'>
+            <LoginView />
           </Route>
 
-          <Route
+          {/* <Route
             exact
             path="/registration"
           >
@@ -106,26 +102,13 @@ function App() {
               // Otherwise, show the registration page
               <RegisterPage />
             }
-          </Route>
-
-          <Route
-            exact
-            path="/home"
-          >
-            {user.id ?
-              // If the user is already logged in,
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the Landing page
-              <LandingPage />
-            }
-          </Route>
+          </Route> */}
 
           {/* If none of the other routes matched, we will show a 404. */}
           <Route>
             <h1>404</h1>
           </Route>
+
         </Switch>
         {/* <Footer /> */}
       </div>
--- src/components/LoginView/LoginView.jsx
+++ src/components/LoginView/LoginView.jsx
@@ -1,46 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 import './LoginView.css';
 
-function LoginPage() {
+function LoginView() {
 
-  const history = useHistory();
+    // Define Library Variables
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    // Define Local States
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    // Login Button Function
+    function handleLoginButton(event) {
+        event.preventDefault();
+        if (username && password) {
+            dispatch({
+                type: 'LOGIN',
+                payload: {
+                    username: username,
+                    password: password,
+                },
+            });
+            history.push('/user')
+        } else {
+            alert('Please populate all required fields.')
+            dispatch({ type: 'LOGIN_INPUT_ERROR' });
+        }
+    };
+
+    // Register Button Function
+    function handleRegisterButton (){
+        history.push('/register');
+    }
 
   return (
     <div>
-        <Grid container id='grid-container-background' direction='column' justifyContent='center' alignItems='center' spacing={2}>
+        <form onSubmit={handleLoginButton}>
 
-            <Grid item id='grid-item-inputs' xs={6}>
+            <Grid container id='grid-container-background' direction='column' justifyContent='flex-start' alignItems='center' spacing={2}>
 
-                <Grid container id='grid-container-inputs' direction='column' spacing={2}>
-
-                    <Grid item>
-                        <TextField label="Email" variant="outlined" />
+                <Grid item id='grid-item-inputs'>
+                    <Grid container direction='column' spacing={2}>
+                        <Grid item>
+                            <TextField required value={username} onChange={(event) => setUsername(event.target.value)} label='Email' />
+                        </Grid>
+                        <Grid item>
+                            <TextField required value={password} onChange={(event) => setPassword(event.target.value)} type='password' label='Password' />
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <TextField type='password' label="Password" variant="outlined" />
-                    </Grid>
-
                 </Grid>
 
-            </Grid>
-
-            <Grid item id='grid-item-buttons' xs={4}>
-                <Grid container id='grid-container-buttons'>
+                <Grid item id='grid-item-buttons'>
+                    <Grid container direction='row' spacing={2}>
+                        <Grid item>
+                            <Button variant='outlined' onClick={handleRegisterButton}>Register</Button>
+                        </Grid>
+                        <Grid item>
+                            <Button variant='contained'>Log In</Button>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
 
+            <input type='submit' hidden />
 
-
-
-
-        </Grid>
+        </form>
     </div>
   );
 }
 
-export default LoginPage;
+export default LoginView;
