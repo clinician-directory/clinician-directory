8be1bb2e8eda7058a5d2bcdda01faad7c705a79c
Kelsey Brown <kbrown55347@gmail.com>
2022-02-01 14:51:32 -0600

    created seedData file and added sample data for user, providers and appointments table

---
+++ seedData.sql
@@ -0,0 +1,23 @@
+-- Sample data
+
+INSERT INTO "user"
+  ("username", "password")
+  VALUES
+  ('testpatient@email.com', '$2a$10$V7dIK6uoviuxbyjDphwewOQYRxIpPu50CzlbqkTOykty09xuRvRSW'); --password: 123
+
+-------------------------------------------------------------
+
+INSERT INTO "providers"
+  ("first_name", "last_name", "specialty", "telemedicine", "city", "health_system", "address", "state", "zip_code")
+  VALUES
+  ('Olivia', 'Smith', 'PM&R', 'true', 'St. Cloud', 'CentraCare', '1900 CentraCare Circle St. Cloud', 'MN', '56303'),
+  ('Emma', 'Johnson', 'Orthopedics', 'true', 'St. Cloud', 'CentraCare', '811 2nd Street Southeast Little Falls', 'MN', 'MN 56345');
+
+-------------------------------------------------------------
+
+INSERT INTO "appointments"
+  ("user_id", "start_time", "end_time", "provider_id", "description")
+  VALUES
+  ('1', '2022-04-10 12:30:00', '2022-04-10 13:00:00', '2', 'ankle pain'); 
+
+-------------------------------------------------------------
