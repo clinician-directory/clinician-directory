56b1f5412160dabf40ae86b16383251f21c93890
whoguy24 <whoguy24@gmail.com>
2022-02-01 11:53:29 -0600

    Installed and imported MUI modules, and created some text fields on the login page.

--- src/components/LoginView/LoginView.jsx
+++ src/components/LoginView/LoginView.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 
 import './LoginView.css';
 
@@ -11,7 +12,32 @@ function LoginPage() {
 
   return (
     <div>
-        <Grid container id='login-card'>
+        <Grid container id='grid-container-background' direction='column' justifyContent='center' alignItems='center' spacing={2}>
+
+            <Grid item id='grid-item-inputs' xs={6}>
+
+                <Grid container id='grid-container-inputs' direction='column' spacing={2}>
+
+                    <Grid item>
+                        <TextField label="Email" variant="outlined" />
+                    </Grid>
+                    <Grid item>
+                        <TextField type='password' label="Password" variant="outlined" />
+                    </Grid>
+
+                </Grid>
+
+            </Grid>
+
+            <Grid item id='grid-item-buttons' xs={4}>
+                <Grid container id='grid-container-buttons'>
+                </Grid>
+            </Grid>
+
+
+
+
+
         </Grid>
     </div>
   );
