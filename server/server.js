f0eb078d4490a853866f3531560eec07024ebff7
Selam Alem <selamtalem@gmail.com>
2022-02-01 13:26:10 -0600

    Providers route fixed

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const providersRouter = require('./routes/providers.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
