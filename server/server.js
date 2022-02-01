af38d82a861e47595f3808e350db453410c6815e
Yasir Uddin <yasir.uddin@icloud.com>
2022-02-01 11:23:59 -0700

    set up starting code for availabilities route on server

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const availabilitiesRouter = require('./routes/availabilities.router.js');
 
 // Body parser middleware
 app.use(bodyParser.json());
