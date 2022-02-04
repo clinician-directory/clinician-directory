d69968ac00198fd7f3a14432ece25968a88774cb
Kelsey Brown <kbrown55347@gmail.com>
2022-02-04 09:46:49 -0600

    created and imported css file for Calendar Month page

--- src/components/CalendarMonth/CalendarMonth.jsx
+++ src/components/CalendarMonth/CalendarMonth.jsx
@@ -5,6 +5,8 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import Navigation from '../Navigation/Navigation';
+// import css file
+import './CalendarMonth.css';
 
 function CalendarMonth() {
 
