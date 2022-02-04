59b26bae27a0208bd848221a725749ff6702d68c
whoguy24 <whoguy24@gmail.com>
2022-02-04 12:52:03 -0600

    Made minor changes to CalendarDay component.

--- src/components/CalendarDay/CalendarDay.jsx
+++ src/components/CalendarDay/CalendarDay.jsx
@@ -2,34 +2,36 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-
-
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
+import dayGridPlugin from '@fullcalendar/timegrid';
 import FullCalendar from '@fullcalendar/react';
 
-// import './LoginView.css';
+import './CalendarDay.css';
 
 function CalendarDay() {
 
   return (
     <div>
 
+        <Grid container>
+            <Grid item id='calendar-day'>
+                <FullCalendar
+                    plugins={[ dayGridPlugin ]}
+                    weekends={true}
+                    // initialView='dayGridWeek'
+                    events={[
+                        { title: 'event 1', date: '2019-04-01' },
+                        { title: 'event 2', date: '2019-04-02' }
+                    ]}
+                />
+            </Grid>
+        </Grid>
 
-        <FullCalendar
-            plugins={[ dayGridPlugin ]}
-            initialView="dayGridDay"
-            weekends={true}
-            events={[
-                { title: 'event 1', date: '2019-04-01' },
-                { title: 'event 2', date: '2019-04-02' }
-            ]}
-        />
+       
 
 
     </div>
