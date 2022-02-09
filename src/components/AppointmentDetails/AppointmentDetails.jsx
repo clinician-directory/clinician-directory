409539abbec3ba84c55af257933c320d285fd1b6
Kelsey Brown <kbrown55347@gmail.com>
2022-02-09 10:26:52 -0600

    added back button to appt details page and functionality to send user back to calendar page on click

--- src/components/AppointmentDetails/AppointmentDetails.jsx
+++ src/components/AppointmentDetails/AppointmentDetails.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
+// MUI imports
+import { Button, Grid } from '@mui/material';
 // import css page
 import './AppointmentDetails.css';
 
@@ -9,6 +11,8 @@ function AppointmentDetails() {
   // params so we can access appointment by id on this page
   const params = useParams();
   const dispatch = useDispatch();
+  // Define history in order to route to page
+  const history = useHistory();
   // access appt details reducer
   const appointmentDetails = useSelector(store => store.appointmentDetailsReducer);
   console.log('appointmentDetails', appointmentDetails);
@@ -21,19 +25,50 @@ function AppointmentDetails() {
     });
   }, []);
 
-  return (
-    <div className="card">
-      <h1>Appointment Details</h1>
+  // handle click of back button
+  const handleBackClick = () => {
+    // send user back to calendar
+    history.push('/calendar');
+    // clear reducer
+    dispatch({
+      type: 'CLEAR_APPOINTMENT_DETAILS'
+    });
+  };
 
-      <p>Provider: {appointmentDetails.first_name} {appointmentDetails.last_name}</p>
-      <p>Location: {appointmentDetails.address}</p>
-      <p>Start: {appointmentDetails.start_time}</p>
-      <p>End: {appointmentDetails.end_time}</p>
-      <p>Description: {appointmentDetails.description}</p>
+  return (
+    <div>
+      <div className="card">
+        <h1>Appointment Details</h1>
+
+        <p>Provider: {appointmentDetails.first_name} {appointmentDetails.last_name}</p>
+        <p>Location: {appointmentDetails.address}</p>
+        <p>Start: {appointmentDetails.start_time}</p>
+        <p>End: {appointmentDetails.end_time}</p>
+        <p>Description: {appointmentDetails.description}</p>
+
+      </div>
+
+      {/* back button */}
+      <div>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-evenly"
+          alignItems="center"
+        >
+          <Button
+            variant="contained"
+            onClick={handleBackClick}
+          >
+            Back
+          </Button>
+        </Grid>
+      </div>
 
       <Navigation />
 
     </div>
+
   );
 };
 
