13d26c053542eccd98103cdd4051bf829a20336f
Selam Alem <selamtalem@gmail.com>
2022-02-10 11:43:26 -0600

    Compelted post route

--- src/redux/sagas/appointments.saga.js
+++ src/redux/sagas/appointments.saga.js
@@ -19,6 +19,23 @@ function* fetchUserAppointments() {
   }
 };
 
+// Saga to post a scheduled appointment created
+function* scheduleAppointment(action) {
+  console.log(' THIS scheduleAppointment action:', action);
+  try {
+      axios({
+          method: 'POST',
+          url: '/api/appointments',
+          data: action.payload
+      })
+      yield put ({ type: 'FETCH_USER_APPOINTMENTS'})
+  } catch {
+      console.log('POST error');
+  }
+} 
+
+
+
 // Saga function to GET appointment details for one appointment
 function* fetchAppointmentDetails(action) {
   try {
@@ -35,6 +52,7 @@ function* fetchAppointmentDetails(action) {
     console.error('fetchAppointmentDetails error', err);
   }
 };
+
 
 // Saga function to intercept dispatches
 function* appointmentsSaga() {
